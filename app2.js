a13717df1cae2d31e399de61f82b90c67a9c2a2b
Percyto <pyarangaq@hotmail.com>
2022-05-15 21:41:58 -0500

    My Second commit by Percyto

--- app2.js
+++ app2.js
@@ -2,5 +2,10 @@ function add(x1,x2){
     return x1+x2;
 }
 
+function sus(x1,x2){
+    return x1-x2;
+}
+
 console.log(add(2,1));
-console.log("Esta es una suma");
+console.log(sus(2,1));
+console.log("Esta es una suma y resta");
